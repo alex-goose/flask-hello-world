cb0657d865bb6540b24ef3d5a1b443c39d603221
Alexander Gustafson <algu6965@colorado.edu>
2024-03-27 16:40:45 +0000

    added create route

--- app.py
+++ app.py
@@ -9,6 +9,26 @@ def test():
     conn.close()
     return "Database connection successful!"
 
+@app.route('/db_create')
+def create():
+    conn = psycopg2.connect("postgres://alex_goose_user:YAkvAHGowhcdbAyYsGIuFHWHxVf51FIF@dpg-co24jo0l5elc73d47vt0-a/alex_goose")
+    cur = conn.cursor()
+    
+    # creating table 'Basketball'
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World from Alexander Gustafson is 3308!'
