e637b34e3a341a56aa92a3f9cb326cbd1a026a84
Alexander Gustafson <algu6965@colorado.edu>
2024-03-27 16:35:03 +0000

    added testing route

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+import psycopg2
+
+@app.route('/db_test')
+def test():
+    conn = psycopg2.connect("postgres://alex_goose_user:YAkvAHGowhcdbAyYsGIuFHWHxVf51FIF@dpg-co24jo0l5elc73d47vt0-a/alex_goose")
+    conn.close()
+    return "Database connection successful!"
+
 @app.route('/')
 def hello_world():
     return 'Hello World from Alexander Gustafson is 3308!'
